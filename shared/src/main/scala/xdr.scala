327cc280231ad42edc3f60c6214806e9e3e63c2b
Cheng Kuan <ck@codeck.org>
2015-12-09 18:51:07 +0800

    to be continued

--- shared/src/main/scala/xdr.scala
+++ shared/src/main/scala/xdr.scala
@@ -3,27 +3,48 @@ package org.strllar.scalaxdr
 package rfc4506 {
 //refer http://tools.ietf.org/html/rfc4506.html
 
-trait XDRDataType //section 4
-trait XDRInteger //section 4.1
-trait XDRUnsignedInteger //section 4.2
-trait XDREnumeration //section 4.3
-trait XDRBoolean //section 4.4
-trait XDRHyper //section 4.5
-trait XDRUnsignedHyper
-trait XDRFloat //section 4.6
-trait XDRDouble //section 4.7
-trait XDRQuadruple //section 4.8
-trait XDRFixedLengthOpaque //section 4.9
-trait XDRVariableLengthOpaque //section 4.10
-trait XDRString //section 4.11
-trait XDRFixedLengthArray //section 4.12
-trait XDRVariableLengthArray //section 4.13
-trait XDRStructure //section 4.14
-trait XDRUnion //section 4.15
-trait XDRVoid //section 4.16
-trait XDRConstant //section 4.17
-trait XDRTypedef //section 4.18
-trait XDROptional //section 4.19
+trait XDRIdentifier {val ident :String} //identifier in section 6.3
+trait XDRConstant {val value :String} //constant in section 6.3
+sealed trait XDRDeclaration {val name :String} //declaration in section 6.3
+sealed trait XDRValue //value in section 6.3
+sealed trait XDRTypeSpecifier //type-specifier in section 6.3
+sealed trait XDRTypeDef{val name :String} //type-def in section 6.3
+
+//concrete XDRValue
+case class XDRConstantValue(value :String) extends XDRValue with XDRConstant
+case class XDRIdentifierValue(ident :String) extends XDRValue with XDRIdentifier
+
+//concrete XDRDeclaration
+case class XDRRegularDeclaration(name :XDRIdentifier, typespec :XDRTypeSpecifier) extends XDRDeclaration
+case class XDRFixedLengthArray(name :XDRIdentifier,typespec :XDRTypeSpecifier, length :XDRValue) extends XDRDeclaration //section 4.12
+case class XDRVariableLengthArray(name :XDRIdentifier,typespec :XDRTypeSpecifier, maxlength :Option[XDRValue]) extends XDRDeclaration //section 4.13
+case class XDRFixedLengthOpaque(name :XDRIdentifier, length :XDRValue) extends XDRDeclaration //section 4.9
+case class XDRVariableLengthOpaque(name :XDRIdentifier, maxlength :Option[XDRValue]) extends XDRDeclaration //section 4.10
+case class XDRString(name :XDRIdentifier, maxlength :Option[XDRValue]) extends XDRDeclaration //section 4.11
+case class XDROptional(name :XDRIdentifier,typespec :XDRTypeSpecifier) //section 4.19
+case class XDRVoid() extends XDRDeclaration {val name = ""}//section 4.16
+
+//concrete XDRTypeSpecifier
+case object XDRInteger extends XDRTypeSpecifier  //section 4.1
+case object XDRUnsignedInteger extends XDRTypeSpecifier //section 4.2
+case class XDRHyper(signed :Boolean) extends XDRTypeSpecifier //section 4.5
+case object XDRFloat extends XDRTypeSpecifier //section 4.6
+case object XDRDouble extends XDRTypeSpecifier  //section 4.7
+case object XDRQuadruple extends XDRTypeSpecifier //section 4.8
+case object XDRBoolean extends XDRTypeSpecifier //section 4.4
+case class XDREnumeration() extends XDRTypeSpecifier //section 4.3
+case class XDRStructure() extends XDRTypeSpecifier //section 4.14
+case class XDRUnion() extends XDRTypeSpecifier //section 4.15
+case class XDRIdentifierTypeSpecifier(ident :String) extends XDRTypeSpecifier  with XDRIdentifier
+
+//XDRTypeDef
+case class XDRRegularTypedef(val decl :XDRDeclaration) extends XDRTypeDef { val name = decl.name}//section 4.18
+//case class  //TODO
+//case class XDRConstant(value :String) ////section 4.17
+
+
+
+
 
 }
 
--- src/main/scala/xdr_gen.scala
+++ src/main/scala/xdr_gen.scala
@@ -1,9 +1,108 @@
 package org.strllar.scalaxdr
 
+import org.parboiled2.RuleTrace.ZeroOrMore
+
 object xdr_generator {
   import org.parboiled2._
+
+  class XDRParser(val input: ParserInput) extends Parser {
+    import CharPredicate.{Digit, Digit19, HexDigit}
+    def WhiteSpaceChar = CharPredicate(" \n\r\t\f")
+    def QuoteBackslash = CharPredicate("\"\\")
+    def QuoteSlashBackSlash = QuoteBackslash ++ "/"
+    def WhiteSpace = rule { zeroOrMore(WhiteSpaceChar) }
+
+    // the root rule
+    def XDRFiles = rule { WhiteSpace ~ Identifier ~ EOI }
+
+    def Identifier = rule {""} //todo
+    def DecimalConstant = rule {""} //todo
+    def HexadecimalConstant = rule {""} //todo
+    def OctalConstant = rule {""} //todo
+
+    def Declaration :Rule0 = rule {(
+      (TypeSpecifier ~ Identifier) |
+      (TypeSpecifier ~ Identifier ~ "[" ~ Value ~ "]") |
+      (TypeSpecifier ~ Identifier ~ "<" ~ optional(Value) ~ ">") |
+      ("opaque" ~ Identifier ~ "[" ~ Value ~ "]") |
+      ("opaque" ~ Identifier ~ "<" ~ optional(Value) ~ ">") |
+      ("string" ~ Identifier ~ "<" ~ optional(Value) ~  ">") |
+      (TypeSpecifier ~ "*" ~ Identifier) |
+      ("void")
+    )}
+
+
+    def Value = rule { Constant | Identifier }
+
+    def Constant = rule { DecimalConstant | HexadecimalConstant | OctalConstant }
+
+    def TypeSpecifier = rule {
+      (optional("unsigned") ~ "int") |
+      (optional("unsigned") ~ "hyper") |
+        ("float") |
+        ("double") |
+        ("quadruple") |
+        ("bool") |
+        EnumTypeSpec |
+        StructTypeSpec |
+        UnionTypeSpec |
+        Identifier
+    }
+
+    def EnumTypeSpec = rule {"enum" ~ EnumBody }
+    def EnumBody = rule {
+      "{" ~
+      ( Identifier ~ "=" ~ Value ) ~
+      zeroOrMore("," ~ Identifier ~ "=" ~ Value) ~
+       "}"
+    }
+
+    def StructTypeSpec = rule {"struct" ~ StructBody }
+    def StructBody = rule {
+      "{" ~
+        (Declaration ~ ";") ~
+        zeroOrMore(Declaration ~ ";") ~
+        "}"
+    }
+
+    def UnionTypeSpec = rule {"union" ~ UnionBody}
+    def UnionBody = rule {
+      "switch" ~ "(" ~ Declaration ~ ")" ~ "{" ~
+        CaseSpec ~
+        zeroOrMore( CaseSpec) ~ optional("default" ~ ":" ~ Declaration ~ ";") ~
+        "}"
+    }
+
+    def CaseSpec = rule {
+      ( "case" ~ Value  ~ ":") ~
+        zeroOrMore( "case" ~ Value ~ ":") ~
+        Declaration ~ ";"
+    }
+
+    def ConstantDef = rule { ("const" ~ Identifier ~ "=" ~ Constant ~ ";") }
+
+    def TypeDef = rule {
+      ("typedef" ~ Declaration ~ ";") |
+        ("enum" ~ Identifier ~ EnumBody ~ ";") |
+        ("struct" ~ Identifier ~ StructBody ~ ";") |
+        ("union" ~ Identifier ~ UnionBody ~ ";")
+    }
+
+    def Definition = rule { TypeDef | ConstantDef }
+
+    def Specification = rule { zeroOrMore(Definition) }
+
+  }
+
+  def run() {
+    val input = scala.io.Source.fromFile("../../../xdr/Stellar-types.x").mkString
+    print(input)
+    val parser = new XDRParser(input)
+    val result = parser.XDRFiles.run()
+    println(result)
+  }
 }
 
 object XDRParser extends App {
-  println("hello xdr")
+  xdr_generator.run()
 }
