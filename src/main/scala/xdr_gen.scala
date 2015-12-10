61a9fbff879ab87cc64ff94ffa8a36757e0a2de8
Cheng Kuan <ck@codeck.org>
2015-12-10 19:53:43 +0800

    parser code complete but not work

--- src/main/scala/xdr_gen.scala
+++ src/main/scala/xdr_gen.scala
@@ -1,24 +1,36 @@
 package org.strllar.scalaxdr
 
-import org.parboiled2.RuleTrace.ZeroOrMore
-
 object xdr_generator {
   import org.parboiled2._
 
-  class XDRParser(val input: ParserInput) extends Parser {
+  trait exactRules {self: Parser =>
     import CharPredicate.{Digit, Digit19, HexDigit}
+
     def WhiteSpaceChar = CharPredicate(" \n\r\t\f")
-    def QuoteBackslash = CharPredicate("\"\\")
-    def QuoteSlashBackSlash = QuoteBackslash ++ "/"
     def WhiteSpace = rule { zeroOrMore(WhiteSpaceChar) }
+    def Newline = rule { "\r\n" | "\n" }
+    def MultilineComment: Rule0 = rule { "/*" ~ zeroOrMore(MultilineComment | !"*/" ~ ANY) ~ "*/" }
+    def Comment: Rule0 = rule {
+      MultilineComment |
+        "//" ~ zeroOrMore(!Newline ~ ANY) ~ &(Newline | EOI)
+    }
+    def WL = rule{ zeroOrMore(WhiteSpaceChar | Comment) }
+
+  }
+
+  class XDRParser(val input: ParserInput) extends Parser with exactRules {
+    import CharPredicate.{Digit, Digit19, HexDigit}
+
+    implicit private[this] def wspStr(s: String): Rule0 = rule { str(s) ~ WL  }
+    implicit private[this] def wspChar(s: Char): Rule0 = rule { ch(s) ~WL }
 
     // the root rule
-    def XDRFiles = rule { WhiteSpace ~ Identifier ~ EOI }
+    def XDRFiles = rule { WL ~ "namespace" ~  Identifier ~ "{" ~ Specification ~ "}" ~ EOI }
 
-    def Identifier = rule {""} //todo
-    def DecimalConstant = rule {""} //todo
-    def HexadecimalConstant = rule {""} //todo
-    def OctalConstant = rule {""} //todo
+    def Identifier = rule {CharPredicate.Alpha ~ zeroOrMore(CharPredicate.AlphaNum | ch('_')) ~ WL }
+    def DecimalConstant = rule(oneOrMore(Digit))
+    def HexadecimalConstant =  rule { "0x" ~ oneOrMore(HexDigit) }
+    def OctalConstant = rule( "0" ~ oneOrMore("0" | ("1" - "7")))
 
     def Declaration :Rule0 = rule {(
       (TypeSpecifier ~ Identifier) |
@@ -94,12 +106,21 @@ object xdr_generator {
 
   }
 
+  import scala.util.Failure
+
   def run() {
     val input = scala.io.Source.fromFile("../../../xdr/Stellar-types.x").mkString
     print(input)
     val parser = new XDRParser(input)
     val result = parser.XDRFiles.run()
     println(result)
+    result match {
+      case Failure(error :ParseError) => {
+        println(parser.formatError(error, new ErrorFormatter(showTraces = true)))
+
+      }
+      case _ => {}
+    }
   }
 }
 
